51b4dbf8568524271b8bc6e72edfd8e6aac9e13f
Adam <adam@sightreadingfactory.com>
2024-03-22 19:23:47 -0400

    parsing all waypoints

--- src/main/scala/surus/DailyMiles.scala
+++ src/main/scala/surus/DailyMiles.scala
@@ -1,18 +1,112 @@
 package surus
 
+import surus.DailyMiles.{allPoints, point}
+import surus.ImplicitConversions.RichU
+
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-object DailyMiles extends App {
-  //allPaths(point("Yellow Creek Road"), point("Max Patch Parking"), maxDays = 5).foreach(route => println(format(route) + "\n\n\n"))
-  allPaths(point("Fontana Dam Visitor Center"), point("Max Patch Parking"), minDay = 10, maxDay = 16, maxLastDay = 10).foreach(route => println(format(route, LocalDate.of(2023, 4, 30)) + "\n\n\n"))
 
-  def allPaths(start: TrailPoint, stop: TrailPoint, minDay: Int, maxDay: Int, maxLastDay: Int): Seq[Seq[TrailPoint]] = {
-    paths(start, stop, allPoints, direction = math.signum(stop.mile.toInt - start.mile.toInt), minDay = minDay, maxDay = maxDay, maxLastDay = maxLastDay)
-      .filter(path => path.contains(stop))
-    //.filter(_.length == maxDays + 1) //x days + the 2 termini
+object DailyMiles extends App {
+  //allPaths(point("Fontana Dam Visitor Center"), point("Max Patch Parking"), minDay = 10, maxDay = 16, maxLastDay = 10).foreach(route => println(format(route, LocalDate.of(2023, 4, 30)) + "\n\n\n"))
+  allPaths(
+    start = point("Undermountain Road/Salisbury CT"),
+    end = point("Silver Hill Campsite"),
+    //start = point("Bear Mountain"),
+    minDay = 10, maxDay = 20, maxLastDay = 8)
+    .foreach(route => println(format(route, LocalDate.of(2023, 9, 23)) + "\n\n\n"))
+
+  def allPaths(start: TrailPoint, end: TrailPoint, minDay: Int, maxDay: Int, maxLastDay: Int): Seq[Seq[TrailPoint]] = {
+    paths(start, end, adjustAllPoints(start, end, allPoints), minDay = minDay, maxDay = maxDay, maxLastDay = maxLastDay)
+     //r .filter(path => path.contains(end))
   }
 
+  def paths(curr: TrailPoint, end: TrailPoint, allPoints: Seq[TrailPoint], minDay: Int, maxDay: Int, maxLastDay: Int): Seq[Seq[TrailPoint]] = {
+    if (curr == end) {
+      Seq(Seq(end))
+    } else {
+      allPoints.filter { p =>
+        val d = curr.distance(p)
+        d > 0 && d >= minDay && d <= maxDay
+      }
+        .flatMap { p =>
+          paths(curr = p, end = end, allPoints = allPoints.dropWhile(_ != p).drop(1), minDay = minDay, maxDay = maxDay, maxLastDay = maxLastDay)
+        }
+    }
+  }
+
+  case class Trip(start: TrailPoint, end: TrailPoint, all: Seq[TrailPoint])
+
+  def adjustAllPoints(start: TrailPoint, end: TrailPoint, allPoints: Seq[TrailPoint]): Seq[TrailPoint] = {
+    val direction = if (end.mile >= start.mile) 1 else -1
+    val (low, high) = if (direction == 1) (start, end) else (end, start)
+    allPoints
+      .dropWhile(_ != low)
+      .takeWhile(_ != high)
+      .:+(high)
+      .sortBy(_.mile * direction)
+      .into { ps =>
+        ps.map(p => p.copy(mile = math.abs(p.mile - ps.head.mile)))
+      }
+  }
+
+
+  //  private def paths(start: TrailPoint, end: TrailPoint, allPoints: Seq[TrailPoint], minDay: Int, maxDay: Int, maxLastDay: Int): Seq[Seq[TrailPoint]] = {
+  //    // Determine the direction: 1 for northbound, -1 for southbound
+  //    val direction = if (end.mile >= start.mile) 1 else -1
+  //
+  //    def validNextPoints(current: TrailPoint): Seq[TrailPoint] = {
+  //      val potentialNextPoints = allPoints
+  //        .dropWhile(_ != current)
+  //        .tap(x => println(s"DailyMiles.scala:30 dropped $x"))
+  //        .takeWhile { point =>
+  //          val dist = direction * (point.mile - current.mile)
+  //          println(s"taking $current => $point = $dist $point $end")
+  //          point == current ||
+  //            (dist >= minDay && dist <= (if (point == end) maxLastDay else maxDay))
+  //        }
+  //        .tap(x => println(s"DailyMiles.scala:36 taked $x"))
+  //      potentialNextPoints.sortBy(_.mile * direction).tap(x => println(s"DailyMiles.scala:49 validNextPoints  $x"))
+  //    }
+  //
+  //    def findPaths(current: TrailPoint, currentPath: Seq[TrailPoint]): Seq[Seq[TrailPoint]] = {
+  //      if (current == end) {
+  //        Seq(currentPath :+ end)
+  //      } else {
+  //        val nextPoints = validNextPoints(current)
+  //        if (nextPoints.isEmpty)
+  //          Seq()
+  //        else
+  //          nextPoints.flatMap(point => findPaths(point, currentPath :+ point))
+  //      }
+  //    }
+  //
+  //    findPaths(current = start, currentPath = Seq(start)).tap(x => println(s"DailyMiles.scala:63 $start, $end => $x"))
+  //  }
+
+  //  private def paths(start: TrailPoint, end: TrailPoint, allPoints: Seq[TrailPoint], minDay: Int, maxDay: Int, maxLastDay: Int): Seq[Seq[TrailPoint]] = {
+  //    if (start.distance(end) < maxLastDay) {
+  //      println(s"done: ${Seq(start, end)} => ${start.distance(end)}")
+  //      Seq(Seq(start, end))
+  //    } else {
+  //      println(s"a $start: $allPoints")
+  //      allPoints
+  //        .filter { p =>
+  //          val dist = start.distance(p)
+  //          (dist >= minDay && dist <= maxDay).tap(x => println(s"DailyMiles.scala:44 is $p ($dist) in range of $start? $x"))
+  //        }
+  //        .tap(x => println(s"DailyMiles.scala:44  $x"))
+  //        .flatMap { next =>
+  //          val pathsFromHere = paths(start = next, end = end, allPoints = allPoints, minDay = minDay, maxDay = maxDay, maxLastDay = maxLastDay)
+  //          if (pathsFromHere.nonEmpty) {
+  //            pathsFromHere.map(pathFromHere => Seq(start) ++ pathFromHere)
+  //          }
+  //          else {
+  //            Seq(Seq(start, next))
+  //          }
+  //        }
+  //    }
+  //  }
   private def format(route: Seq[TrailPoint], startDate: LocalDate): String = {
     route.sliding(2).toList
       .zip(Iterator.iterate(startDate)(_.plusDays(1)).map(d => d.format(DateTimeFormatter.ofPattern("EEE M-d"))))
@@ -26,25 +120,6 @@ object DailyMiles extends App {
       }
   }
 
-  private def paths(start: TrailPoint, end: TrailPoint, allPoints: Seq[TrailPoint], direction: Int, minDay: Int, maxDay: Int, maxLastDay: Int): Seq[Seq[TrailPoint]] = {
-    if (end.mile - start.mile < maxLastDay) {
-      Seq(Seq(start, end))
-    } else {
-      allPoints
-        .dropWhile(p => p.mile - start.mile <= minDay)
-        .takeWhile(p => p.mile - start.mile <= maxDay && p != end)
-        //.tap(x => println(s"DailyMiles.scala:36 $start => $x"))
-        .flatMap { next =>
-          val pathsFromHere = paths(start = next, end = end, allPoints = allPoints, direction = direction, minDay = minDay, maxDay = maxDay, maxLastDay = maxLastDay)
-          if (pathsFromHere.nonEmpty) {
-            pathsFromHere.map(pathFromHere => Seq(start) ++ pathFromHere)
-          }
-          else {
-            Seq(Seq(start, next))
-          }
-        }
-    }
-  }
 
   private lazy val pointForName = allPoints.map(p => p.name.trim -> p).toMap
 
@@ -52,44 +127,28 @@ object DailyMiles extends App {
 
   lazy val allPoints = {
     """
-      |158.4	Yellow Creek Road
-      |165.9  Fontana Dam Shelter
-      |166.3  Fontana Dam Visitor Center
-      |172.3  Campsite
-      |177.7	Mollies Ridge
-      |180.8	Russell Field Shelter
-      |183.7	Spence Field Shelter
-      |189.8	Derrick Knob Shelter
-      |195.5	Silers Bald Shelter
-      |197.2	Double Spring Gap Shelter
-      |202.8	Mt Collins Shelter
-      |207.7	Newfound Gap
-      |210.8	Icewater Spring
-      |218.2	Pecks Corner
-      |223.4	Tri-Corner Knob
-      |231.1	Cosby Knob
-      |238	Davenport Gap Shelter
-      |241.8	Standing Bear Hostel
-      |243.9	Painter Branch Campsite
-      |248.7	Groundhog Creek
-      |254.4	Max Patch Parking
-      |257	Roaring Fork
-      |260.5	Lemon Gap
-      |261.8	Walnut Mountain
-      |263.2	campsite atop small knoll
-      |268.3	Garenflo Gap/1173
-      |271.7	Deer Park Mountain
-      |274.9	Hot Springs
-      |279.1	Campsite
-      |283.2	Campsite
-      |285.9	Spring Mountain
-      |294.5	Little Laurel
-      |301.8	Jerry Cabin
-      |308.5	Flint Mountain
-      |315.1	Campsite
-      |317.3	Hogback Ridge
-      |319.7	Sam's Gap
-      |"""
+      |1404.1	Bear Mountain
+      |1408.9	Hemlock Springs Campsite
+      |1426.9	Clarence Fahenstock State Park
+      |1430.6	Shenandoah Tenting Area
+      |1431.9	RPH Shelter
+      |1440.9	Morgan Stewart Shelter
+      |1448.7	Telepone Pioneers Shelter
+      |1451.8	Pawling/Wingdale NY
+      |1457.5	Wiley Shelter
+      |1461.5	Ten Mile River Shelter
+      |1467	Schagticoke Mountain Campsite
+      |1469.9	Mt Algo Shelter
+      |1477.2	Stewart Hollow Brook Shelter
+      |1477.8	Stony Brook Campsite
+      |1480.5	Silver Hill Campsite
+      |1483.9	Caesar Brook Campsite
+      |1487.2	Pine Swamp Brook Shelter
+      |1489.6	Sharon Mountain Campsite
+      |1492.5	Belters Campsite
+      |1498.5	Limestone Spring Shelter
+      |1502.9	Undermountain Road/Salisbury CT
+      """
       .stripMargin.trim
       .split("\n").toSeq
       .map { line =>
@@ -101,6 +160,11 @@ object DailyMiles extends App {
   }
 
   case class TrailPoint(mile: Double, name: String) {
+    def distance(other: TrailPoint): Double = {
+      other.mile - mile
+    }
+
     override lazy val toString = s"[$name ($mile)]"
   }
 }
+
--- src/main/scala/surus/WikiTrailsParser.scala
+++ src/main/scala/surus/WikiTrailsParser.scala
@@ -5,31 +5,39 @@ import org.jsoup.Jsoup
 import scala.jdk.CollectionConverters._
 import scala.util.chaining.scalaUtilChainingOps
 
+object ParseAllWaypoints extends App {
+
+}
 object WikiTrailsParser extends App {
-  Jsoup.connect("http://www.wikitrail.org/sections/view/at/3011/us-321-to-damascus")
-    .get
-    .select("[^data-role]").asScala
-    .map(element => element.wholeText())
-    .filter(_.trim.startsWith("Sleep"))
-    .map { wholeText =>
-      wholeText
-        .split("\n").toList
-        .map(_.trim).filter(_.nonEmpty)
-        .drop(1)
-        .map(parseWaypoint)
-    }
-    .tap(x => println(s"WikiTrailsParser.scala:11  $x"))
-  case class Waypoint(mile: Double, name: String, distanceOffTrail: Option[String])
+private val sectionIds = 3001 to 3044
+  sectionIds.map { sectionId =>
+    Jsoup.connect(s"http://www.wikitrail.org/sections/view/at/$sectionId/")
+      .get
+      .select("span.ui-li-icon").asScala //<span class='ui-li-icon restaurant16' title='Restaurant' width='16' height='16'></span>
+      //.tap(x => println(s"WikiTrailsParser.scala:18  ${x.mkString("\n")}")
+      .filter(e => e.attr("title").nonEmpty) //"Add Shelter"
+      .map(element => (parseWaypoint(element.parent().wholeText().trim, element.attr("title").trim)))
+      .distinct
+      .sortBy(_.mile)
+      .foreach { wp =>
+        import wp._
+        println(s"$mile, $name, $distanceOffTrail, $wpType")
+      }
+  }
+
+  case class Waypoint(mile: Double, name: String, distanceOffTrail: Option[String], wpType: String)
+
 
   //426.8: Lake House [2.0W]
-  def parseWaypoint(wp: String): Waypoint = {
+  def parseWaypoint(wp: String, wpType: String): Waypoint = {
     Waypoint(
       mile = wp.takeWhile(_ != ':').toDouble,
       name = wp.dropWhile(_ != ':').drop(2).takeWhile(_ != '[').trim,
       distanceOffTrail = {
         val raw = wp.dropWhile(_ != '[').drop(1).takeWhile(_ != ']').trim
         if (raw.nonEmpty) Some(raw) else None
-      }
+      },
+      wpType = wpType
     )
   }
 }
